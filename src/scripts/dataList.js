6c472a672d3a546d886d54991a928e20d81dd01c
Alex <alex.holder@bigfootds.com>
2024-08-27 20:35:12 +1000

    feat: can remove items from list

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -16,35 +16,65 @@ let dataArray = [
 // Find elements on the page for us to use in the logic 
 let carsContainerElement = document.getElementById("carsContainer");
 
-// Create elements to add to the page via the logic 
-let carsContainerList = document.createElement("ul");
 
-// Do logic for each individual item in the array 
-dataArray.forEach((car) => {
+function renderData(){
 
-	// Verify what we are working with, just log it to see the data's value 
-	console.log(car);
+	carsContainerElement.innerText = "";
 
-	// Create a new HTML element to help us format the data's value
-	let newCarEntry = document.createElement("li");
-	// Add the data to the new element 
-	newCarEntry.innerText += car;
+	// Create elements to add to the page via the logic 
+	let carsContainerList = document.createElement("ul");
 
+	// Do logic for each individual item in the array 
+	dataArray.forEach((car) => {
 
-	// Add a button to each entry
-	// click on the button to remove the entry from the list 
-	let removeButton = document.createElement("button");
-	// removeButton.innerText = "Remove " + car;
-	removeButton.innerText = `Remove ${car}`;
-	// Add the removeButton to the car entry 
-	newCarEntry.appendChild(removeButton);
+		// Verify what we are working with, just log it to see the data's value 
+		console.log(car);
+
+		// Create a new HTML element to help us format the data's value
+		let newCarEntry = document.createElement("li");
+		// Add the data to the new element 
+		newCarEntry.innerText += car;
 
 
-	// Add the nicely-formatted element into the list container 
-	carsContainerList.appendChild(newCarEntry);
-});
+		// Add a button to each entry
+		// click on the button to remove the entry from the list 
+		let removeButton = document.createElement("button");
+		// removeButton.innerText = "Remove " + car;
+		removeButton.innerText = `Remove ${car}`;
 
-// Add the list container onto the actual HTML page 
-carsContainerElement.appendChild(carsContainerList);
+		removeButton.onclick = (() => removeCarFromDataList(car));
+
+		// Add the removeButton to the car entry 
+		newCarEntry.appendChild(removeButton);
+
+
+		// Add the nicely-formatted element into the list container 
+		carsContainerList.appendChild(newCarEntry);
+	});
+
+	// Add the list container onto the actual HTML page 
+	carsContainerElement.appendChild(carsContainerList);
+
+}
+
+
+
+
+
+
+function removeCarFromDataList (targetItemToRemove) {
+	
+	// Remove the target item from the dataArray
+	dataArray = dataArray.filter((car) => car != targetItemToRemove);
+
+	// After the filtering is done, re-render the page with the "new" array
+	renderData();
+
+	
+}
+
+
+
+
 
 
