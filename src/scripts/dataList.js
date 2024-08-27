834b3fc50b1be28e47338fd975eb5a46e54f8b22
Alex <alex.holder@bigfootds.com>
2024-08-27 19:34:07 +1000

    feat: html and js skeleton in place

---
+++ src/scripts/dataList.js
@@ -0,0 +1,18 @@
+let dataArray = [
+	"warthog",
+	"interceptor",
+	"herbie",
+	"lightning mcqueen",
+	"batmobile",
+	"bat tumbler",
+	"cybertruck",
+	"delorean",
+	"elanore",
+	"magic school bus",
+	"toyota corolla"
+];
+
+dataArray.forEach((plants) => {
+	console.log(plants);
+});
+
