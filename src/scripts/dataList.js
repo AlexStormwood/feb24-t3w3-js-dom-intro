1e2c60809dc9d0281c0bc7c9e3c57328825fb542
Alex <alex.holder@bigfootds.com>
2024-08-29 20:36:07 +1000

    fix: validation on form input

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -75,6 +75,19 @@ function removeCarFromDataList (targetItemToRemove) {
 
 
 function addCarToDataList(event, targetInputId){
+
+	// Find the form element 
+	let formElement = document.getElementById("carsInputForm");
+	// Use the form element.checkValidity() method and save the result 
+	let isFormValid = formElement.checkValidity();
+	console.log("isFormValid value: " + isFormValid);
+	// do a conditional based on that result value 
+	if (!isFormValid){
+		formElement.reportValidity();
+		return;
+	} 
+
+
 	// Find the form from the event 
 	// prevent the form from doing its default behaviour (refreshing the page)
 	event.preventDefault();
@@ -85,6 +98,9 @@ function addCarToDataList(event, targetInputId){
 	// Grab the string value from the text field 
 	console.log(targetTextInput.value);
 
+	// Chalani's request: alert after submit 
+	// alert("Submitted a new entry: " + targetTextInput.value);
+
 	// Push the string value into dataArray
 	dataArray.push(targetTextInput.value);
 
@@ -93,6 +109,9 @@ function addCarToDataList(event, targetInputId){
 
 	// Focus on the text input field again to enable quick data entry!
 	targetTextInput.focus();
+
+	// Chalani's request: alert after submit 
+	alert("Submitted a new entry: " + dataArray[dataArray.length - 1]);
 
 	// call renderData() to update the page 
 	renderData();
